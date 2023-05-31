24aac05e79adeff43e627bb97cb9be6c896dd0fd
Milton Torres <Miltontorres018@gmail.com>
2023-05-31 10:31:26 -0600

    se agrego la configuracion de application.properties y se realizo un ejemplo de request con el servidor

--- src/main/java/com/matr/api_front_back/controller/UsuarioController.java
+++ src/main/java/com/matr/api_front_back/controller/UsuarioController.java
@@ -4,10 +4,13 @@ package com.matr.api_front_back.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//se hace la notacion de que sera un clase de tipo controler
+//de igual forma se crea en automatico el bean de esta clase
+//para posteriormente el contenedor pueda gestionarla
 @RestController
 public class UsuarioController {
 
-
+//se comienzan a crear los endpoints con los cuales el cliente hara los requests
     @GetMapping(value = "prueba")
     public String prueba(){
         return "prueba controller";
