6771426fb4dbe1e36fd7fe18a12512a765d7ae7d
Milton Torres <Miltontorres018@gmail.com>
2023-05-31 11:26:44 -0600

    se agrego el endpoint "listausuario" se hizo un request desde backend y se mostro una lista con 3 usuarios en formato json

--- src/main/java/com/matr/api_front_back/controller/UsuarioController.java
+++ src/main/java/com/matr/api_front_back/controller/UsuarioController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //se hace la notacion de que sera un clase de tipo controler
 //de igual forma se crea en automatico el bean de esta clase
 //para posteriormente el contenedor pueda gestionarla
@@ -39,5 +42,39 @@ public class UsuarioController {
         usuario.setTelefono("72727262");
         usuario.setPassword("1234");
         return usuario;
+    }
+    @GetMapping(value = "listausuario")
+    public List<Usuario> getListusuario() {
+
+        List<Usuario> listausuarios = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("milton");
+        usuario.setApellido("torres");
+        usuario.setEmail("mil@gmail.com");
+        usuario.setTelefono("72727262");
+        usuario.setPassword("1234");
+
+        Usuario usuario2= new Usuario();
+        usuario2.setId(2L);
+        usuario2.setNombre("gala");
+        usuario2.setApellido("romero");
+        usuario2.setEmail("gala@gmail.com");
+        usuario2.setTelefono("4222323");
+        usuario2.setPassword("1432232");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3L);
+        usuario3.setNombre("mak");
+        usuario3.setApellido("romero");
+        usuario3.setEmail("mak@gmail.com");
+        usuario3.setTelefono("92826222");
+        usuario3.setPassword("11111");
+
+        listausuarios.add(usuario);
+        listausuarios.add(usuario2);
+        listausuarios.add(usuario3);
+        return listausuarios;
     }
 }
