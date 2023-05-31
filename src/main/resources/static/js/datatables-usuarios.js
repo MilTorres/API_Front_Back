4e53c948ed3f861eb50419217ec40088b2bd00c3
Milton Torres <Miltontorres018@gmail.com>
2023-05-31 12:13:26 -0600

    se agrego el endpoint "listausuario" se hizo un request desde Frontend y se mostro una lista con 3 usuarios impresa en una tabla datos que estamos subiendo en Java backend y mostrando en html por medio de javascript

--- src/main/resources/static/js/datatables-usuarios.js
+++ src/main/resources/static/js/datatables-usuarios.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
 async function cargarusuarios() {
 
-    const request = await fetch('usuario/144', {
+    const request = await fetch('listausuario', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
@@ -22,4 +22,28 @@ async function cargarusuarios() {
 
     console.log(usuarios);
 
+    let listadohtml = '';
+    for (let usuario of usuarios){
+        let formatousuario= ' <tr>\n' +
+            '                                    <td>1</td>\n' +
+            '                                    <td>'+usuario.nombre+'</td>\n' +
+            '                                    <td>'+usuario.apellido+'</td>\n' +
+            '                                    <td>'+usuario.email+'</td>\n' +
+            '                                    <td>'+usuario.telefono+'</td>\n' +
+            '                                    <td>'+usuario.contraseña+'</td>\n' +
+            '                                    <td><a href="#" class="btn btn-danger btn-circle btn-sm">\n' +
+            '                                        <i class="fas fa-trash"></i>\n' +
+            '                                    </a></td>\n' +
+            '                                </tr>';
+
+       listadohtml += formatousuario;
+    }
+
+
+
+
+
+//esta linea modifica el componente selecionado en este caso #tusuarios tbody(que es su cuerpo de la tabla
+// y lo remplaza por el campo o varibale que le demos
+    document.querySelector('#tusuarios tbody').outerHTML = listadohtml;
 }
